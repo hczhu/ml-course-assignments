5b740267fca281369707ec3c26b03647d123411c
Kezun Sima <simakezun@outlook.com>
2018-04-14 23:57:56 -0700

    add files

--- stanford-cs229/ica/bellsej.py
+++ stanford-cs229/ica/bellsej.py
@@ -9,6 +9,7 @@
 ### On windows:
 ###      pip install pyaudio sounddevice
 ###
+# Algorithm: http://cs229.stanford.edu/notes/cs229-notes11.pdf
 
 import sounddevice as sd
 import numpy as np
@@ -20,37 +21,40 @@ def normalize(dat):
 
 def load_data():
     mix = np.loadtxt('data/mix.dat')
+    print(mix.shape)
     return mix
 
 def play(vec):
     sd.play(vec, Fs, blocking=True)
 
+def sigmoid(x):
+    return 1.0 / (1 + np.exp(-x.clip(-100, 100)))
+
 def unmixer(X):
     M, N = X.shape
-    W = np.eye(N)
+    W = np.eye(N, dtype=float)
 
     anneal = [0.1, 0.1, 0.1, 0.05, 0.05, 0.05, 0.02, 0.02, 0.01, 0.01,
               0.005, 0.005, 0.002, 0.002, 0.001, 0.001]
     print('Separating tracks ...')
     ######## Your code here ##########
-
+    for epoch in range(len(anneal)):
+        lr = anneal[epoch]
+        for i in range(M):
+            W += lr * (np.linalg.inv(W.T) + np.outer(1 - 2 * sigmoid(W.dot(X[i, :])), X[i, :]))
     ###################################
     return W
 
 def unmix(X, W):
-    S = np.zeros(X.shape)
-
-    ######### Your code here ##########
-
-    ##################################
-    return S
+    # S = np.zeros(X.shape)
+    return np.matmul(X, W.T)
 
 def main():
     X = normalize(load_data())
 
     for i in range(X.shape[1]):
         print('Playing mixed track %d' % i)
-        play(X[:, i])
+        # play(X[:, i])
 
     W = unmixer(X)
     S = normalize(unmix(X, W))
